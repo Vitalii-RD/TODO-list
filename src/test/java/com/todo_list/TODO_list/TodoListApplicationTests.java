fcea4674c3b7fe6cb53a1bbea6ce6033c90dd0c1
Vitaliy5 <dydvitrom@ukr.net>
2020-12-21 21:22:31 +0200

    git init

---
+++ src/test/java/com/todo_list/TODO_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo_list.TODO_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
