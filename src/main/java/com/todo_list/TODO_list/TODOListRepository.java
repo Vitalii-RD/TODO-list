813e4d8d2e86a9b6544169bddf6ee44a516faa43
Vitaliy5 <dydvitrom@ukr.net>
2020-12-22 10:09:52 +0200

    add TaskController and repository fro tasks

---
+++ src/main/java/com/todo_list/TODO_list/TODOListRepository.java
@@ -0,0 +1,56 @@
+package com.todo_list.TODO_list;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class TODOListRepository {
+  private final AtomicLong counter = new AtomicLong();
+  private Map<Long, Task> tasks;
+
+  public TODOListRepository(Map<Long, Task> tasks) {
+    this.tasks = tasks;
+  }
+
+  public TODOListRepository() {
+    tasks = new HashMap<>();
+  }
+
+  public Collection<Task> getTasks() {
+    return tasks.values();
+  }
+
+  public Task put(Task task) {
+    long id = counter.incrementAndGet();
+    Task newTask = new Task(id, task.getTitle(), task.getText(), task.isDone(), task.getDueDate());
+    tasks.put(id, newTask);
+    return newTask;
+  }
+
+  public Task update(Task task, long id) {
+    Task oldTask = tasks.getOrDefault(id, null);
+    if (oldTask == null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
+
+    oldTask.setTitle(task.getTitle());
+    oldTask.setText(task.getText());
+    oldTask.setDone(task.isDone());
+    oldTask.setDueDate(task.getDueDate());
+
+    oldTask.updateLastUpdated();
+
+    return oldTask;
+  }
+
+  public void delete(long id) {
+    Task oldTask = tasks.getOrDefault(id, null);
+    if (oldTask == null)
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
+
+    tasks.remove(id);
+  }
+}
---
+++ src/main/java/com/todo_list/TODO_list/TaskController.java
@@ -0,0 +1,33 @@
+package com.todo_list.TODO_list;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.Collection;
+
+@RestController
+public class TaskController {
+  private TODOListRepository repository = new TODOListRepository();
+
+  @GetMapping("/tasks")
+  public Collection<Task> getTasks() {
+    return repository.getTasks();
+  }
+
+  @PostMapping("/tasks")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Task putTask(@RequestBody Task task) {
+    return repository.put(task);
+  }
+
+  @PutMapping("/tasks/{id}")
+  public Task updateTask(@RequestBody Task task, @PathVariable("id") long id) {
+    return repository.update(task, id);
+  }
+
+  @DeleteMapping("/tasks/{id}")
+  public void deleteTask(@PathVariable("id") long id) {
+    repository.delete(id);
+  }
+}
