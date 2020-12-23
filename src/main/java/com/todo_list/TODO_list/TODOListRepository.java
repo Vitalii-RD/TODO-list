57f6a227cfcfc7c75eb6d8dc8da61821bfb592ab
Vitaliy5 <dydvitrom@ukr.net>
2020-12-23 12:20:53 +0200

    change TODOListRepository to work with a database.

--- src/main/java/com/todo_list/TODO_list/TODOListRepository.java
+++ src/main/java/com/todo_list/TODO_list/TODOListRepository.java
@@ -3,54 +3,103 @@ package com.todo_list.TODO_list;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.*;
 
 public class TODOListRepository {
-  private final AtomicLong counter = new AtomicLong();
-  private Map<Long, Task> tasks;
-
-  public TODOListRepository(Map<Long, Task> tasks) {
-    this.tasks = tasks;
-  }
+  private JDBC jdbc;
+  private final String table;
 
   public TODOListRepository() {
-    tasks = new HashMap<>();
+    this("todos");
   }
 
-  public Collection<Task> getTasks() {
-    return tasks.values();
+  public TODOListRepository(String table) {
+    try {
+      jdbc = new JDBC();
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+    this.table = table;
+  }
+
+  public List<Task> getTasks() {
+    List<Task> tasks = new LinkedList<>();
+
+    try (ResultSet rs = jdbc.executeQuery("SELECT * FROM "+table+";")) {
+      while (rs.next()) {
+        tasks.add(getTask(rs));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return tasks;
+  }
+
+  private Task getTask(ResultSet rs) throws SQLException {
+    LocalDateTime created = rs.getTimestamp("datecreated").toLocalDateTime();
+    LocalDateTime updated = rs.getTimestamp("dateupdated").toLocalDateTime();
+    LocalDateTime dueDate = rs.getTimestamp("duedate").toLocalDateTime();
+
+    return new Task(rs.getInt("id"), rs.getString("title"), rs.getString("text"),
+            rs.getBoolean("done"), created, updated, dueDate);
   }
 
   public Task put(Task task) {
-    long id = counter.incrementAndGet();
-    Task newTask = new Task(id, task.getTitle(), task.getText(), task.getDone(), task.getDueDate());
-    tasks.put(id, newTask);
-    return newTask;
+    return put(task, table);
   }
 
-  public Task update(Task task, long id) {
-    Task oldTask = tasks.getOrDefault(id, null);
-    if (oldTask == null)
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
-
-    oldTask.setTitle(task.getTitle());
-    oldTask.setText(task.getText());
-    oldTask.setDone(task.getDone());
-    oldTask.setDueDate(task.getDueDate());
-
-    oldTask.updateLastUpdated();
-
-    return oldTask;
+  public Task put(Task task, String table) {
+    try {
+      int id = jdbc.putTask(task, table);
+      return get(id);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Smth went wrong :(");
+    }
   }
 
-  public void delete(long id) {
-    Task oldTask = tasks.getOrDefault(id, null);
-    if (oldTask == null)
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
+  public Task get(int id) {
+    try (ResultSet rs = jdbc.executeQuery("SELECT * FROM "+table+" WHERE id = " + id + ";")){
+      if (rs.next()) return getTask(rs);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "id was wrong");
+    }
+    return null;
+  }
 
-    tasks.remove(id);
+  public Task update(Task task, int id) {
+    String query = "UPDATE "+table+" " +
+            "SET title = '" + task.getTitle() + "', text = '" + task.getText() + "', " +
+            "done = " + task.isDone() + ", duedate = '" + task.getDueDate() + "', " +
+            "dateupdated = '" + new Timestamp(new Date().getTime()) + "' " +
+            "WHERE id = " + id +";";
+
+    try {
+      if (get(id) != null) {
+        jdbc.executeUpdate(query);
+        return get(id);
+      } else throw  new SQLException();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
+    }
+  }
+
+  public void delete(int id) {
+    String query = "DELETE FROM "+table+" WHERE id = " + id + ";";
+
+    try {
+      if (get(id) != null) jdbc.executeUpdate(query);
+      else throw new SQLException();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Task was not found");
+    }
   }
 }
