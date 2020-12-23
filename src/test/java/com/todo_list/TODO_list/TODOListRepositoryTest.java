88b7835d2c2e019976641e902084a26f8a98c0b9
Vitaliy5 <dydvitrom@ukr.net>
2020-12-23 12:21:21 +0200

    change tests to work with a db

--- src/test/java/com/todo_list/TODO_list/TODOListRepositoryTest.java
+++ src/test/java/com/todo_list/TODO_list/TODOListRepositoryTest.java
@@ -1,24 +1,28 @@
 package com.todo_list.TODO_list;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
+import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TODOListRepositoryTest {
-  private TODOListRepository repository;
+  private static TODOListRepository repository;
 
-  @BeforeEach
-  public void setUp() {
-    repository = new TODOListRepository();
+  @BeforeAll
+  public static void init() throws SQLException, ClassNotFoundException {
+    new JDBC().executeUpdate("CREATE TABLE test (" +
+            "id serial not null," +
+            "title varchar(50) not null," +
+            "text varchar(200) not null," +
+            "done boolean not null," +
+            "datecreated timestamp not null," +
+            "dateupdated timestamp not null," +
+            "duedate timestamp not null);");
+
+    repository = new TODOListRepository("test");
   }
 
   @Test
@@ -26,54 +30,67 @@ class TODOListRepositoryTest {
     Assertions.assertEquals(0, repository.getTasks().size());
   }
 
+  @Test
   void WhenAdded__getTasks__returnsAddedTasks() {
-    Task t1 = new Task(1, "title1", "text1", false, new Date());
-    Task t2 = new Task(2, "title2", "text2", true, new Date());
-    List<Task> tasks = new ArrayList<>();
+    Task t1 = new Task(1, "title1", "text1", false,
+            LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
+    Task t2 = new Task(2, "title2", "text2", true,
+            LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
 
-    tasks.add(t1);
-    tasks.add(t2);
-    repository.put(t1);
-    repository.put(t2);
+    Task t3 = repository.put(t1);
+    Task t4 = repository.put(t2);
 
-    Assertions.assertIterableEquals(tasks, repository.getTasks());
+    Assertions.assertEquals(t3.getId(), repository.getTasks().get(0).getId());
+    Assertions.assertEquals(t4.getId(), repository.getTasks().get(1).getId());
   }
 
   @Test
   void WhenInvoked__put__addsTask() {
-    Task t1 = new Task(1, "title","text1", false, new Date());
-    List<Task> tasks = new ArrayList<>();
+    Task t1 = new Task(1, "title","text1", false, LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now());
 
     Task newTask = repository.put(t1);
-    tasks.add(newTask);
 
-    Assertions.assertIterableEquals(tasks, repository.getTasks());
+    Assertions.assertEquals(newTask.getId(), repository.getTasks().get(0).getId());
   }
 
   @Test
   void update() {
-    Task oldTask = new Task(1, "title1", "text", false, new Date());
-    Task newTask = new Task(1, "title1", "new text", true, new Date());
+    LocalDateTime dueDate  = LocalDateTime.now();
+    Task oldTask = new Task(1, "title", "text", false,
+            LocalDateTime.now(), LocalDateTime.now(), dueDate);
+    Task newTask = new Task(1, "new title", "new text", true,
+            LocalDateTime.now(), LocalDateTime.now(),dueDate);
 
     Task addedTask = repository.put(oldTask);
-    repository.update(newTask, addedTask.getId());
+    Task updatedTask = repository.update(newTask, addedTask.getId());
+    String pattern = "dd/MM/yyyy HH:mm:ss";
+    String date1 =  newTask.getDueDate().format(DateTimeFormatter.ofPattern(pattern));
+    String date2 =  updatedTask.getDueDate().format(DateTimeFormatter.ofPattern(pattern));
 
-    Assertions.assertEquals(newTask.getText(), addedTask.getText());
-    Assertions.assertEquals(newTask.getDone(), addedTask.getDone());
-    Assertions.assertEquals(newTask.getDueDate(), addedTask.getDueDate());
+    assertEquals(newTask.getText(), updatedTask.getText());
+    assertEquals(newTask.isDone(), updatedTask.isDone());
+    assertEquals(date1, date2);
   }
 
   @Test
   void WhenInvoked__delete__removesTask() {
-    Task t1 = new Task(1, "title", "text", false, new Date());
+    Task t1 = new Task(1, "title", "text", false,
+            LocalDateTime.now(), LocalDateTime.now(),LocalDateTime.now());
+    int sizeBefore = repository.getTasks().size();
 
-    repository.put(t1);
-    repository.delete(1);
+    Task addedTask = repository.put(t1);
+    repository.delete(addedTask.getId());
 
-    Assertions.assertEquals(0, repository.getTasks().size());
+    assertEquals(sizeBefore, repository.getTasks().size());
   }
 
-  public void tearDown() {
-    repository = null;
+  @AfterEach
+  public void cleanUp() throws SQLException, ClassNotFoundException {
+    new JDBC().executeUpdate("DELETE FROM test;");
+  }
+
+  @AfterAll
+  public static void tearDown() throws SQLException, ClassNotFoundException {
+    new JDBC().executeUpdate("DROP TABLE test;");
   }
 }
